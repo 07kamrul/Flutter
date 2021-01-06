3bfa3605d8052cf2df89bcae0a7f7d92b2e391cb
Farid <w3farid@gmail.com>
2021-01-06 16:12:08 +0600

    form multiple  values show

--- form_app/lib/showForm.dart
+++ form_app/lib/showForm.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 
-
-class DisplayApp extends StatelessWidget{
+class DisplayApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Form',
       home: DisplayValue(),
@@ -11,55 +10,102 @@ class DisplayApp extends StatelessWidget{
   }
 }
 
-class DisplayValue extends StatefulWidget{
-
+class DisplayValue extends StatefulWidget {
   @override
   _DisplayValueState createState() => _DisplayValueState();
-
 }
 
-class _DisplayValueState extends State<DisplayValue>{
+class _DisplayValueState extends State<DisplayValue> {
   final nameController = TextEditingController();
   final emailController = TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
     nameController.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar:AppBar(
-        title:Text("Form"),
+      appBar: AppBar(
+        title: Text("Form"),
       ),
       body: Padding(
+
         padding: const EdgeInsets.all(20.0),
-        child: TextField(
-          controller: nameController,
-        ),
-        //
-        // child: new Form(
-        //   child: new ListView(
-        //     children: [],
-        //   ),
+        // child: TextField(
+        //   controller: nameController,
         // ),
+        //
+        child: new Form(
+          child: new ListView(
+            children: <Widget>[
+              new TextFormField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                decoration: new InputDecoration(
+                  hintText: 'Enter your name...',
+                  labelText: 'Name',
+                ),
+              ),
+              new TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: new InputDecoration(
+                  hintText: 'Enter your email...',
+                  labelText: 'Email',
+                ),
+              ),
+              new Container(
+                width: screenSize.width,
+                child: new RaisedButton(
+                    child: new Text(
+                    'Show Input Values',
+                      style: new TextStyle(color: Colors.white),
+                    ),
+                    onPressed:(){
+                      return showDialog(
+                          context: context,
+                          builder: (context){
+                            return AlertDialog(
+                              content:Container(
+                                height: 100,
+                                child: Column(
+                                children: [
+                                  Text(nameController.text),
+                                  Text(emailController.text)
+                                ],
+                                ),
+                              ),
+                            );
+                          },
+                      );
+                    },
+                  color: Colors.blue,
+
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          return showDialog(
-              context: context,
-            builder: (context){
-                return AlertDialog(content:Text(nameController.text),
-                );
-            },
-          );
-        },
-        tooltip:'Show the value',
-        // child: Icon(Icon.text_fields),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     return showDialog(
+      //       context: context,
+      //       builder: (context) {
+      //         return AlertDialog(
+      //           content: Text(nameController.text),
+      //         );
+      //       },
+      //     );
+      //   },
+      //   tooltip: 'Show the value',
+      //   // child: Icon(Icon.text_fields),
+      // ),
     );
   }
-
 }
