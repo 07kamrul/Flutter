1102ca0faada8ed17b3bea2fd7d5c0948da48485
07kamrul <mdkamrulhasanewu@gmail.com>
2021-01-23 13:29:46 +0600

    State management in todo application

---
+++ todo_app/lib/screen/todo_model_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:todo_app/model/task_model.dart';
+
+class TodoModel extends ChangeNotifier{
+
+  List<TaskModel> taskList = [];
+
+  addTaskInList(){
+    TaskModel taskModel = TaskModel("title ${taskList.length}", "this is the task no detail ${taskList.length}");
+    taskList.add(taskModel);
+
+    notifyListeners();
+  }
+}
