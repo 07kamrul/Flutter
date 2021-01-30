1b427611886e4b17827ae0bf184d6b9f21692909
07kamrul <mdkamrulhasanewu@gmail.com>
2021-01-30 15:20:58 +0600

    Bloc Login App

--- flutter_bloc_concept/lib/main.dart
+++ flutter_bloc_concept/lib/main.dart
@@ -12,16 +12,15 @@ class MyApp extends StatelessWidget {
     return BlocProvider<CounterCubit>(
       create: (context) => CounterCubit(),
       child: MaterialApp(
-        title: 'Flutter Bloc Pattern',
-        theme: ThemeData(
-          primarySwatch: Colors.pink,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: BlocProvider<CounterCubit>(
-          create: (context) => CounterCubit(),
-          child: MyHomePage(title: 'Bloc Pattern Home Page'),
-        )
-      ),
+          title: 'Flutter Bloc Pattern',
+          theme: ThemeData(
+            primarySwatch: Colors.pink,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: BlocProvider<CounterCubit>(
+            create: (context) => CounterCubit(),
+            child: MyHomePage(title: 'Bloc Pattern Home Page'),
+          )),
     );
   }
 }
@@ -43,78 +42,77 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocConsumer<CounterCubit, CounterState>(
-                listener: (context, state) {
-                  if (state.wasIncremented == true) {
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text('Incremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ));
-                  }
-                  else if (state.wasIncremented == false) {
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text('Decremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ));
-                  }
-                },
-                builder: (context, state) {
-                  if (state.counterValue < 0) {
-                    return Text(
-                      'Negative No: ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue != 0 &&
-                      state.counterValue % 2 == 0) {
-                    return Text(
-                      'YAAH ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue == 7) {
-                    return Text(
-                      'Birthday ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else {
-                    return Text(
-                      'Positive No: ' + state.counterValue.toString(),
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  }
-                },
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).decrement();
-                    },
-                    tooltip: 'Decrement',
-                    child: Icon(Icons.remove),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      // context.bloc<CounterCubit>().increament();
-                      BlocProvider.of<CounterCubit>(context).increament();
-                    },
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ),
-                ],
-              )
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text('Incremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else if (state.wasIncremented == false) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text('Decremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'Negative No: ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue != 0 &&
+                    state.counterValue % 2 == 0) {
+                  return Text(
+                    'YAAH ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue == 7) {
+                  return Text(
+                    'Birthday ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else {
+                  return Text(
+                    'Positive No: ' + state.counterValue.toString(),
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+              },
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    // context.bloc<CounterCubit>().increament();
+                    BlocProvider.of<CounterCubit>(context).increament();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )
+          ],
         ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
---
+++ login_form_using_bloc/android/app/src/main/kotlin/com/cts/login_form_using_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cts.login_form_using_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
