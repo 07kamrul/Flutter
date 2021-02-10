e1a50e1c6d4ba56619a1ddb3268799369f0f97dd
07kamrul <mdkamrulhasanewu@gmail.com>
2021-02-10 19:08:04 +0600

    Flutter Bloc Access Navigator

---
+++ flutter_bloc_concept/lib/presentation/router/app_router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_concept/presentation/screens/home_screen.dart';
+import 'package:flutter_bloc_concept/presentation/screens/second_screen.dart';
+
+class AppRouter{
+  Route onGenerateRoute(RouteSettings settings){
+    final GlobalKey<ScaffoldState> key = settings.arguments;
+
+    switch(settings.name){
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) => HomeScreen(
+              title: "Home Screen",
+              color:  Colors.blueAccent,
+            ),
+        );
+      case '/second':
+        return MaterialPageRoute(
+          builder: (_) => SecondScreen(
+            title: "Second Screen",
+            color:  Colors.blueAccent,
+            homeScreenKey: key,
+          ),
+        );
+    }
+  }
+}
